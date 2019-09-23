4f2d9592aa2e08fccdeeed9cb51819927708f21e
ZhuzhouNo3 <907130054@qq.com>
2019-09-23 13:35:57 +0800

    .

--- sendPubKey.sh
+++ sendPubKey.sh
@@ -1,13 +1,1 @@
-'''
-Pkey = yourPubKey
-ssh root@xxx "bash sendPubKey.sh $Pkey"
-'''
-if test ! -e .ssh;
-then mkdir .ssh;
-chmod 700 .ssh;
-fi;
-if test ! -e ".ssh/authorized_keys";
-then touch ".ssh/authorized_keys";
-chmod 600 ".ssh/authorized_keys";
-fi;
-echo $1 >> ".ssh/authorized_keys";
+ssh $1 "if test ! -e .ssh;then mkdir .ssh;chmod 700 .ssh;fi;if test ! -e \".ssh/authorized_keys\";then touch \".ssh/authorized_keys\";chmod 600 \".ssh/authorized_keys\";fi;echo ${@: 2} >> \".ssh/authorized_keys\";"
