8b7e2b08ea68cc3df30b98c17a7765eeb152a850
ZhuzhouNo3 <907130054@qq.com>
2019-09-28 00:20:42 +0800

    .

--- foolssr.sh
+++ foolssr.sh
@@ -5,21 +5,21 @@ set password [lindex $argv 1]
 spawn bash
 send "bash ServerSpeederCentOS6.sh\r"
 expect {
-"*Enter*" { send "\r" ; exp_continue }
-"*\[n\]*" { send "\r" ; exp_continue }
-"*\[y\]*" { send "\r" ; exp_continue }
-"*running*" { send "bash ssr.sh" }
+"*Enter*" { send "\r" ; after 300 ; exp_continue }
+"*\[n\]*" { send "\r" ; after 300 ; exp_continue }
+"*\[y\]*" { send "\r" ; after 300 ; exp_continue }
+"*root@*" { send "bash ssr.sh" }
 }
 expect {
-"*\[1-15\]*" { send "1\r" ; exp_continue }
-"*2333)*" { send "$port\r" ; exp_continue }
-"*doub.io)*" { send "$password\r" ; exp_continue }
-"*ctr)*" { send "10\r" ; exp_continue }
-"*v4)*" { send "\r" ; exp_continue }
+"*\[1-15\]*" { send "1\r" ; after 300 ; exp_continue }
+"*2333)*" { send "$port\r" ; after 300 ; exp_continue }
+"*doub.io)*" { send "$password\r" ; after 300 ; exp_continue }
+"*ctr)*" { send "10\r" ; after 300 ; exp_continue }
+"*v4)*" { send "\r" ; after 300 ; exp_continue }
 "*Y/n*" { send "Y\r" }
 }
 expect {
-"*):*" { send "\r" ; exp_continue }
+"*):*" { send "\r" ; after 300 ; exp_continue }
 "*y/N)*" { send "y\r" }
 }
 interact
