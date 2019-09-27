ed56bfbbe3a8b20083a5f5484e035361bef59941
ZhuzhouNo3 <907130054@qq.com>
2019-09-28 00:08:31 +0800

    .

--- foolssr.sh
+++ foolssr.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/expect
 set timeout -1
-
-
+set port [lindex $argv 0]
+set password [lindex $argv 1]
+spawn bash
+send "bash ServerSpeederCentOS6.sh\r"
+expect {
+"*Enter*" send { "\r" ; exp_continue }
+"*\[n\]*" send { "\r" ; exp_continue }
+"*\[y\]*" send { "\r" ; exp_continue }
+"*running*" send { "bash ssr.sh" }
+}
+expect {
+"*\[1-15\]*" send { "1\r" ; exp_continue }
+"*2333)*" send { "$port\r" ; exp_continue }
+"*doub.io)*" send { "$password\r" ; exp_continue }
+"*ctr)*" send { "10\r" ; exp_continue }
+"*v4)*" send { "\r" ; exp_continue }
+"*Y/n*" send { "Y\r" }
+}
+expect {
+"*):*" send { "\r" ; exp_continue }
+"*y/N)*" send { "y\r" }
+}
 interact
--- zssr.sh
+++ zssr.sh
@@ -4,4 +4,6 @@ yum -y install expect
 wget -N --no-check-certificate https://github.com/ZhuzhuNo3/VPS/raw/master/ssr.sh ; chmod -x ssr.sh
 wget -N --no-check-certificate https://github.com/ZhuzhuNo3/VPS/raw/master/ServerSpeederCentOS6.sh ; chmod -x ServerSpeederCentOS6.sh
 wget -N --no-check-certificate https://github.com/ZhuzhuNo3/VPS/raw/master/foolssr.sh ; chmod -x foolssr.sh
-expect foolssr.sh
+port=$(($RANDOM%10000+10000))
+password=$(mkpasswd -l 10)
+expect foolssr.sh $port $password
